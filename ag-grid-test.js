67897a71c8d1be22e3f3d5811743de0f68b81e9b
George Gosline <g.gosline@kew.org>
2016-09-18 18:45:24 +0100

    col headers

--- ag-grid-test.js
+++ ag-grid-test.js
@@ -1,15 +1,58 @@
 agGrid.initialiseAgGridWithWebComponents();
 
 var columnDefs = [
-    {headerName: "Make", field: "make"},
-    {headerName: "Model", field: "model"},
-    {headerName: "Price", field: "price"}
+   {headerName: "Taxonno", field: "taxonNo"},
+{headerName: "Flora", field: "flora"},
+{headerName: "Family", field: "family"},
+{headerName: "Taxon", field: "taxon"},
+{headerName: "Mainsubject", field: "mainsubject"},
+{headerName: "Subject", field: "subject"},
+{headerName: "Subpart", field: "subpart"},
+{headerName: "Category", field: "category"},
+{headerName: "Value", field: "value"},
+{headerName: "Mod", field: "mod"},
+{headerName: "Posit", field: "posit"},
+{headerName: "Phase", field: "phase"},
+{headerName: "Presence", field: "presence"},
+{headerName: "Start", field: "start"},
+{headerName: "End", field: "end"},
 ];
 
 var rowData = [
-    {make: "Toyota", model: "Celica", price: 35000},
-    {make: "Ford", model: "Mondeo", price: 32000},
-    {make: "Porsche", model: "Boxter", price: 72000}
+	{
+		"taxonNo": 666,
+		"flora": null,
+		"family": "testfam",
+		"taxon": "Test run",
+		"mainsubject": "nerve",
+		"subject": "nerve",
+		"subpart": null,
+		"category": "subject",
+		"value": "nerve",
+		"mod": "lateral",
+		"posit": null,
+		"phase": null,
+		"presence": null,
+		"start": 0,
+		"end": 28
+	},
+	{
+		"taxonNo": 666,
+		"flora": null,
+		"family": "testfam",
+		"taxon": "Test run",
+		"mainsubject": "nerve",
+		"subject": "nerve",
+		"subpart": "grouping",
+		"category": "grouping",
+		"value": "pairs",
+		"mod": "8\u201310",
+		"posit": null,
+		"phase": null,
+		"presence": null,
+		"start": 15,
+		"end": 28
+	}
 ];
 
 var gridOptions = {
