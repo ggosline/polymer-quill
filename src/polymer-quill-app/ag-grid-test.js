4d28e97c22af7d09446c49614e0af8a4a1219434
George Gosline <g.gosline@kew.org>
2016-10-13 03:50:50 +0100

    all elements integrated

---
+++ src/polymer-quill-app/ag-grid-test.js
@@ -0,0 +1,70 @@
+"use strict";
+
+var columnDefs = [
+    {headerName: "Taxonno", field: "taxonNo", width: 150},
+    {headerName: "Flora", field: "flora", width: 150},
+    {headerName: "Family", field: "family", width: 150},
+    {headerName: "Taxon", field: "taxon", width: 150},
+    {headerName: "Mainsubject", field: "mainsubject", width: 150},
+    {headerName: "Subject", field: "subject", width: 150},
+    {headerName: "Subpart", field: "subpart", width: 150},
+    {headerName: "Category", field: "category", width: 150},
+    {headerName: "Value", field: "value", width: 150},
+    {headerName: "Mod", field: "mod", width: 150},
+    {headerName: "Posit", field: "posit", width: 150},
+    {headerName: "Phase", field: "phase", width: 150},
+    {headerName: "Presence", field: "presence", width: 150},
+    {headerName: "Start", field: "start", width: 150},
+    {headerName: "End", field: "end", width: 150},
+];
+
+
+
+var rowData = [
+	{
+		"taxonNo": 666,
+		"flora": null,
+		"family": "testfam",
+		"taxon": "Test run",
+		"mainsubject": "nerve",
+		"subject": "nerve",
+		"subpart": null,
+		"category": "subject",
+		"value": "nerve",
+		"mod": "lateral",
+		"posit": null,
+		"phase": null,
+		"presence": null,
+		"start": 0,
+		"end": 28
+	},
+	{
+		"taxonNo": 666,
+		"flora": null,
+		"family": "testfam",
+		"taxon": "Test run",
+		"mainsubject": "nerve",
+		"subject": "nerve",
+		"subpart": "grouping",
+		"category": "grouping",
+		"value": "pairs",
+		"mod": "8\u201310",
+		"posit": null,
+		"phase": null,
+		"presence": null,
+		"start": 15,
+		"end": 28
+	}
+];
+
+
+function rowSelectedFunc(event) {
+    // window.alert("row " + event.node.data.start + " selected = " + event.node.selected);
+    let strt = event.node.data.start;
+    let slen = event.node.data.end - event.node.data.start;
+
+    if (event.node.selected) {
+        quill.formatText(0, quill.getLength(), 'background', false);
+        quill.formatText(strt, slen, 'background', 'rgb(102, 185, 102)');}
+    };
+
